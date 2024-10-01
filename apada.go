08540e299f8e7c9f6f267a1d7d66888f8b906b06
Roman Parker <roman@parker.pm>
2024-10-01 20:10:28 +0200

    moving file and adding execution permission

--- apada.go
+++ apada.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
 )
 
 const desktopTemplate = `[Desktop Entry]
@@ -14,39 +16,76 @@ Terminal=false
 Categories={{.Categories}}`
 
 type DesktopEntry struct {
-    Name       string
-    Exec       string
-    Icon       string
-    Categories string
+	Name       string
+	Exec       string
+	Icon       string
+	Categories string
 }
 
 func main() {
-    if len(os.Args) < 2 {
-        fmt.Println("Usage: ./apada <filename>")
-        os.Exit(1)
-    }
+	// Check if the fileName argument is provided
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: ./apada <filename>")
+		os.Exit(1)
+	}
 
-    fileName := os.Args[1]
-    file, err := readFile(fileName)
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
-    defer file.Close()
+	// Read command-line flags
+	sourceFile := flag.String("file", "", "Path to the source file")
+	flag.Parse()
+
+	fileName := os.Args[1]
+	targetDir := "/home/Applications/AppImages"
+
+	moveFileToTargetDirectory(targetDir, *sourceFile, fileName)
+
+	addExecutionPermission(targetFile)
 }
 
-func readFile(name string) (*os.File, error) {
-    file, err := os.Open(name)
+// moveFileToTargetDirectory handles moving the file to the target directory
+func moveFileToTargetDirectory(targetDir string, sourceFile string, fileName string) {
+	// If no source file is provided, assume it's in the current directory
+	if sourceFile == "" {
+		currentDir, err := os.Getwd() // Get current working directory
+		if err != nil {
+			fmt.Println("Error getting current directory:", err)
+			os.Exit(1)
+		}
 
-    if err != nil {
-        if os.IsNotExist(err) {
-            return nil, fmt.Errorf("file not found")
-        }
-        return nil, err
-    }
-    return file, nil
+		// Set the source file path to the current directory + fileName
+		sourceFile = filepath.Join(currentDir, fileName)
+	}
+
+	// Check if the target directory exists, create it if not
+	if _, err := os.Stat(targetDir); os.IsNotExist(err) {
+		// Create the target directory if it doesn't exist
+		err := os.MkdirAll(targetDir, 0755)
+		if err != nil {
+			fmt.Println("Error creating target directory:", err)
+			os.Exit(1)
+		}
+		fmt.Println("Target directory created:", targetDir)
+	}
+
+	// Define the target file path
+	targetFile := filepath.Join(targetDir, filepath.Base(sourceFile))
+
+	// Move the file to the target directory
+	err := os.Rename(sourceFile, targetFile)
+	if err != nil {
+		fmt.Println("Error moving file:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("File successfully moved to:", targetFile)
+    return targetFile
 }
 
-func addExecutionPermission() {
-
+// addExecutionPermission adds execution permissions to the file
+func addExecutionPermission(filePath string) {
+	err := os.Chmod(filePath, 0755)
+	if err != nil {
+		fmt.Println("Error adding execution permissions to the file:", err)
+		os.Exit(1)
+	}
+	fmt.Println("Execution permissions added to:", filePath)
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/roman-parker/apada
+
+go 1.23.0
